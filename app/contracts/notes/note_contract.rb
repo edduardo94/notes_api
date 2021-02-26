0ec63664057f527add85c38105468ca678c99f77
Eduardo Souza <edduardo94@gmail.com>
2021-02-26 14:43:09 -0300

    removing unecessary stuff

--- app/contracts/notes/note_contract.rb
+++ app/contracts/notes/note_contract.rb
@@ -1,40 +1,11 @@
 module Repos
-    class SearchContract < Dry::Validation::Contract
-      ORDER = %w[asc desc].freeze
-      SORT = %w[updated forks stars].freeze
+    class SearchContract < Dry::Validation::Contract      
   
       params do
         optional(:page).value(:integer)
-        optional(:per_page).value(:integer)
-        optional(:sort).value(:string)
-        optional(:order).value(:string)        
-        optional(:text).value(:string)
+        optional(:per_page).value(:integer)                
       end
-  
-      rule(:order, :sort) do
-        key.failure('only order sorted values') if values[:order].present? && !values[:sort].present?
-      end
-  
-      rule(:sort) do
-        if key? && !(SORT.include? value)
-          key.failure(
-            'must to be stars,forks or update date'
-          )
-        end
-      end
-  
-      rule(:order) do
-        if key? && !(ORDER.include? value)
-          key.failure(
-            'must to be desc or asc '
-          )
-        end
-      end        
-  
-      rule(:text) do
-        key.failure('text must not be to blank') if key? && value.blank?
-      end
-  
+      
       rule(:page) do
         key.failure('must higher than 0 ') if key? && (value < 1)
       end
